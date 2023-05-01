06b91a15105dda0eb415a36616febb0b52a23ae7
SquirrelloDev <olejnike2000@gmail.com>
2023-05-01 23:06:25 +0200

    Added sorting styling

--- src/components/AddTodoForm.tsx
+++ src/components/AddTodoForm.tsx
@@ -5,11 +5,12 @@ import {themeContext} from "@/context/ThemeProvider";
 const AddTodoForm = () => {
     const {isDarkTheme} = useContext(themeContext);
     const formBoxClass = isDarkTheme ? `${formClasses['header__form__input-box']} ${formClasses['header__form__input-box--dark']}` : formClasses['header__form__input-box'];
-  return(
+    const inputClass = isDarkTheme ? `${inputClasses['todo-input']} ${inputClasses['todo-input--dark']}` : inputClasses['todo-input'];
+    return(
       <form className={formClasses.header__form}>
           <div className={formBoxClass}>
               {/*<div className={formClasses['header__form__input-box__circle']}></div>*/}
-              <input type='text' placeholder='Create a new todo..' className={inputClasses['todo-input']}/>
+              <input type='text' placeholder='Create a new todo..' className={inputClass}/>
           </div>
 
       </form>
